0e95be7a81e30ccf5b2edc969cbd3d6566504be8
David <david.ndiba@student.moringaschool.com>
2024-09-25 07:58:08 +0300

    fix(accesscontrol): address issue with access control

--- src/access.ts
+++ src/access.ts
@@ -1,10 +1,12 @@
-export default (initialState: API.UserInfo) => {
+export default (initialState: any) => {
   // 在这里按照初始化数据定义项目中的权限，统一管理
   // 参考文档 https://umijs.org/docs/max/access
-  const canSeeAdmin = !!(
-    initialState && initialState.name !== 'dontHaveAccess'
-  );
   return {
-    canSeeAdmin,
+    admin: initialState?.currentUser?.role_and_permissions?.find?.(
+      (e) => e?.role_name === 'Super Admin',
+    ),
+    editor: initialState?.currentUser?.role_and_permissions?.find?.(
+      (e) => e?.role_name === 'Editors',
+    ),
   };
 };
